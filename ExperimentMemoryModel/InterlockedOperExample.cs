8e8a0ab77504e42088189d81731229e62b1fb806
tkruk <tjk_2@msn.com>
2013-06-29 20:21:11 -0400

    Added new classes

---
+++ ExperimentMemoryModel/InterlockedOperExample.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace ExperimentMemoryModel
+{
+    // Explanation: Intrelocked operations are atomic operations that can reduce locking in multithreaded applications 
+    public class InterlockedOperExample
+    {
+        private int _value = int.MinValue;
+        private object _lock = new object();
+
+        public int IncrementValue()
+        {
+            lock (_lock)
+            {
+                _value++;
+                
+                return _value;
+            }
+        }
+
+        // Using Intelocked.Increment we can rewrite above method
+        // NOTE: Interesting fact about Interlocked methods is that they cannot be reordered with other memory operations.
+        public int IncrementValuePartTwo()
+        {
+            return Interlocked.Increment(ref _value); // this approach may run faster then above example
+        }
+    }
+}
---
+++ ExperimentMemoryModel/SamplePublicationviaThreadingApi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExperimentMemoryModel
+{
+    // Publication via threading API pattern in concurrent programming.
+    public class SamplePublicationviaThreadingApi
+    {
+        static int s_value;
+
+        static void Run()
+        {
+            s_value = 42;
+            Task.Factory.StartNew(() =>
+            {
+                Debug.WriteLine(s_value);
+            });
+        }
+    }
+}
