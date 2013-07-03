259c3f491b1c33d84cc03407e3c2a4e515e05e50
tkruk <tjk_2@msn.com>
2013-07-03 19:22:03 -0400

    Added new classes and unit tests

---
+++ Experiments/Other/PlayingWithLINQ.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Experiments.Other
+{
+    public class PlayingWithLINQ
+    {
+        public Dictionary<int, int[]> ArrayInit(int count)
+        {
+            Dictionary<int, int[]> rtnvValue = new Dictionary<int, int[]>();
+
+            // Init array without loops.
+            int[] a = Enumerable.Repeat(-1, count).ToArray();
+            int[] b = Enumerable.Range(0, count).ToArray();
+            int[] c = Enumerable.Range(0, count).Select(i => 100 + 10 * i).ToArray();
+
+            rtnvValue.Add(1, a);
+            rtnvValue.Add(2, b);
+            rtnvValue.Add(3, c);
+
+            return rtnvValue;
+        }
+
+        public int LoopMultiArraysInOneLoop(int count)
+        {
+            var rtnVal = ArrayInit(count);
+            int keepingCount = 0;
+            
+            // Loop through all arrays without multiple FOR loops
+            // NOTE: Interesting fact about Concat operator is that LINQ will not allocate a new array to hold
+            // elements for all arrays, so this is also memory efficient.
+            foreach (var d in rtnVal[1].Concat(rtnVal[2]).Concat(rtnVal[3]))
+            {
+                keepingCount++;
+                Console.WriteLine("Array value now is: " + d);
+            }
+
+            return keepingCount;
+        }
+
+        public int LoopMultiArraysInOneLoopOnlyUniqueValues(int count)
+        {
+            var rtnVal = ArrayInit(count);
+
+            int keepingCount = 0;
+            // Loop through all arrays without multiple FOR loops - access only unique values.
+            foreach (var d in rtnVal[1].Union(rtnVal[2]).Union(rtnVal[3]))
+            {
+                keepingCount++;
+                Console.WriteLine("Array value now is: " + d);
+            }
+
+            return keepingCount;
+        }
+
+        public IEnumerable<int> GenerateRandomSequence(int count)
+        {
+            Random rand = new Random();
+            var randSeq = Enumerable.Repeat(0, count).Select(i => rand.Next());
+
+            return randSeq;
+        }
+
+        public string GenerateString(int count)
+        {
+            string rtnVal = new string(Enumerable.Range(0, count).Select(i => (char)('A' + i % 3)).ToArray());
+            
+            return rtnVal;
+        }
+    }
+}
---
+++ UnitTestExperiments/Other/UnitTestLINQTricks.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using Experiments.Other;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestExperiments.Other
+{
+    [TestClass]
+    public class UnitTestLINQTricks
+    {
+        [TestMethod]
+        public void TestArrayInitMethod()
+        {
+            var tricks = new PlayingWithLINQ();
+            var rtnValue = tricks.ArrayInit(10);
+
+            Assert.IsNotNull(rtnValue);
+        }
+
+        [TestMethod]
+        public void TestArrayConcat()
+        {
+            var tricks = new PlayingWithLINQ();
+            var rtnval = tricks.LoopMultiArraysInOneLoop(10);
+
+            Assert.AreEqual(30, rtnval);
+        }
+
+        [TestMethod]
+        public void TestArrayUnion()
+        {
+            var tricks = new PlayingWithLINQ();
+            var rtnval = tricks.LoopMultiArraysInOneLoopOnlyUniqueValues(10);
+
+            Assert.AreEqual(21, rtnval);
+        }
+
+        [TestMethod]
+        public void TestRandomSequence()
+        {
+            var tricks = new PlayingWithLINQ();
+            var rtnVal = tricks.GenerateRandomSequence(10);
+
+            Assert.AreEqual(10, rtnVal.Distinct().Count());
+        }
+
+        [TestMethod]
+        public void TestGenerteString()
+        {
+            var tricks = new PlayingWithLINQ();
+            var rtnVal = tricks.GenerateString(10);
+
+            Assert.AreEqual(10, rtnVal.Length);
+        }
+    }
+}
