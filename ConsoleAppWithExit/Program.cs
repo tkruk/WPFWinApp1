168dd72fb4b7a0f4e857018ceb7914920644c168
tkruk <tjk_2@msn.com>
2013-07-05 12:35:56 -0400

    Added new console app with CTRL-C exit

---
+++ ConsoleAppWithExit/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace ConsoleAppWithExit
+{
+    class Program
+    {
+        // NOTE: Console app that exits when user type CTRL-C....
+        private static bool cancel = false;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Application has started . Pressed CTRL-C to end...");
+
+            var mainThread = new Thread(Worker);
+            mainThread.Start();
+
+            var autoResetEvent = new AutoResetEvent(false);
+            Console.CancelKeyPress += (sender, eventArgs) =>
+                {
+                    eventArgs.Cancel = true;
+                    autoResetEvent.Set();
+                };
+
+            autoResetEvent.WaitOne();
+            cancel = true;
+            Console.WriteLine("Shutting down..");
+        }
+
+        private static void Worker()
+        {
+            while (!cancel)
+            {
+                Console.WriteLine("Worker is working...");
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine("Worker thred is done!!!!");
+        }
+    }
+}
