1eb382da7da897298d70fd15fa536b0c0e301b6b
tkruk <tjk_2@msn.com>
2013-07-05 17:46:21 -0400

    Added UnixTime class and tests

---
+++ Experiments/Other/UnixTime.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Experiments.Other
+{
+    public class UnixTime
+    {
+        private static readonly DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        public static long FromDateTime(DateTime dateTime)
+        {
+            if (dateTime.Kind == DateTimeKind.Utc)
+            {
+                return (long)dateTime.Subtract(epoch).TotalSeconds;
+            }
+
+            throw new ArgumentException("Input date time must be in UTC!");
+        }
+
+        public static DateTime ToDateTime(long unixTime)
+        {
+            return epoch.AddSeconds(unixTime);
+        }
+
+        public static long Now
+        {
+            get
+            {
+                return FromDateTime(DateTime.UtcNow);
+            }
+
+        }
+    }
+}
---
+++ UnitTestExperiments/Other/UnitTestUnixTime.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Experiments.Other;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestExperiments.Other
+{
+    [TestClass]
+    public class UnitTestUnixTime
+    {
+        [TestMethod]
+        public void TestNow()
+        {
+            var rtnValue = UnixTime.Now;
+            Console.WriteLine("Current time: " + rtnValue.ToString());
+
+            Assert.IsNotNull(rtnValue);
+        }
+
+        [TestMethod]
+        public void TestConvertFromUnixToUTCDateTime()
+        {
+            var rtnValue = UnixTime.ToDateTime(UnixTime.Now);
+            Console.WriteLine("Converted value from unix to date time: " + rtnValue.ToString());
+
+            Assert.IsNotNull(rtnValue);
+        }
+
+        [TestMethod]
+        public void TestConvertFromUTCDateTimeToUnix()
+        {
+            var rtnValue = UnixTime.FromDateTime(DateTime.UtcNow);
+            Console.WriteLine("Converted value from date time to unix: " + rtnValue.ToString());
+
+            Assert.IsNotNull(rtnValue);
+        }
+
+        [TestMethod]
+        public void TestFromUTCDateTimeToUnixException()
+        {
+            var message = string.Empty;
+
+            try
+            {
+                var localDateTime = new DateTime(10000, DateTimeKind.Local);
+                UnixTime.FromDateTime(localDateTime);
+            }
+            catch (ArgumentException ex)
+            {
+                message = ex.Message;
+            }
+
+            Assert.AreEqual("Input date time must be in UTC!", message);
+        }
+    }
+}
