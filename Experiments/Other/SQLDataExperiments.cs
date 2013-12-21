e8169bc2f0ae1ed2295bc20e32cf3c1f736dc00b
tkruk <tjk_2@msn.com>
2013-12-21 18:58:14 -0500

    Added new files

---
+++ Experiments/Other/SQLDataExperiments.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+using System.Data.SqlClient;
+
+namespace Experiments.Other
+{
+    public class SQLDataExperiments
+    {
+        public SqlConnection DataConnection { get; set; }
+        
+        public SQLDataExperiments()
+        {
+            DataConnection = CreateConnection();
+        }
+
+        public int ExecuteSimpleInsertAccount()
+        {
+            return InsertAccountData("Account 1", "Test");
+        }
+
+        public int ExecuteDeleteAccount()
+        {
+            return DeleteAccountData("Account 1");
+        }
+  
+        private SqlConnection CreateConnection()
+        {
+            var connectionString = "Persist Security Info=True;User ID=sa;Password=test1234;Database=TestDB;Server=.\\SQLSVR2012";
+            var conn = new SqlConnection(connectionString);
+            conn.Open();    
+
+            return conn;
+        }
+
+        private int InsertAccountData(string accountID, string description)
+        {
+            string queryString = String.Format("INSERT INTO ACCOUNT " + "(Account_ID, Description) Values('{0}', '{1}')", accountID, description);
+
+            SqlCommand command = new SqlCommand(queryString, DataConnection);
+            int recordsAffected = command.ExecuteNonQuery();
+
+            return recordsAffected;
+        }
+
+        private int DeleteAccountData(string accountID)
+        {
+            string queryString = String.Format("DELETE FROM ACCOUNT WHERE ACCOUNT_ID = '{0}'", accountID);
+
+            SqlCommand command = new SqlCommand(queryString, DataConnection);
+            int recordsAffected = command.ExecuteNonQuery();
+
+            return recordsAffected;
+        }
+
+        public int DeleteAllAccountData()
+        {
+            string queryString = String.Format("DELETE FROM ACCOUNT");
+
+            SqlCommand command = new SqlCommand(queryString, DataConnection);
+            int recordsAffected = command.ExecuteNonQuery();
+
+            return recordsAffected;
+        }
+
+        public void InsertMultipleAccounts(int count)
+        {
+            var rand = new Random();
+            
+            for (int i = 0; i < count; i++)
+            {
+                var accountId = rand.Next().ToString();
+                var description = accountId;
+
+                InsertAccountData(accountId, description);
+            }
+        }
+
+        public int CountRecordsInAccountTable()
+        {
+            int rtnVal = 0;
+
+            string queryString = "SELECT COUNT(*) FROM ACCOUNT";
+            SqlCommand command = new SqlCommand(queryString, DataConnection);
+
+            rtnVal = Convert.ToInt32(command.ExecuteScalar());
+            
+            return rtnVal;
+        }
+    }
+}
---
+++ UnitTestExperiments/Other/UnitTestSQLDataExperiments.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using Experiments.Other;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestExperiments.Other
+{
+    [TestClass]
+    public class UnitTestSQLDataExperiments
+    {
+        [TestMethod]
+        public void TestSimpleInsert()
+        {
+            var dataExperiments = new SQLDataExperiments();
+
+            var rtnVal = dataExperiments.ExecuteSimpleInsertAccount();
+
+            Assert.AreEqual(1, rtnVal);
+
+            rtnVal = dataExperiments.ExecuteDeleteAccount();
+
+            Assert.AreEqual(1, rtnVal);
+        }
+
+        [TestMethod]
+        public void TestMultipleInsertsTraceWithStopWatch()
+        {
+            Stopwatch watch = new Stopwatch();
+            int count = 100000;
+            var dataExperiments = new SQLDataExperiments();
+
+            watch.Start();
+            dataExperiments.InsertMultipleAccounts(count);
+            watch.Stop();
+
+            Console.WriteLine("Measured time: " + watch.Elapsed.TotalMilliseconds + " ms.");
+
+            var rtnVal = dataExperiments.CountRecordsInAccountTable();
+
+            Assert.AreEqual(count, rtnVal);
+
+            dataExperiments.DeleteAllAccountData();
+            rtnVal = dataExperiments.CountRecordsInAccountTable();
+
+            Assert.AreEqual(0, rtnVal);
+        }
+    }
+}
