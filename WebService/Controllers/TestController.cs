cb1048ea9cd245fd61a57cf311174ba2b1df32dd
tkruk <tjk_2@msn.com>
2014-02-01 13:58:41 -0500

    Added sample code

--- WebService/Controllers/TestController.cs
+++ WebService/Controllers/TestController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Net.Http;
+using System.Web.Configuration;
 using System.Web.Http;
 using System.Web.Http.Cors;
 
@@ -9,20 +10,39 @@ namespace WebService.Controllers
     [EnableCors(origins: "http://localhost:49700", headers: "*", methods: "*")]
     public class TestController : ApiController
     {
-        public HttpResponseMessage Get()
-        {
-            return new HttpResponseMessage()
-            {
-                Content = new StringContent("GET: Test message")
-            };
-        }
-
         public HttpResponseMessage Post()
         {
             return new HttpResponseMessage()
             {
                 Content = new StringContent("POST: Test message")
             };
+        }
+
+        public HttpResponseMessage Get()
+        {            
+            string pwd = string.Empty;
+            string userID = string.Empty;
+
+            var values = Request.GetQueryNameValuePairs().ToDictionary(kv => kv.Key, kv => kv.Value, StringComparer.OrdinalIgnoreCase);
+          
+            values.TryGetValue("userID", out userID);
+            values.TryGetValue("pwd", out pwd);
+
+            var validUserId = WebConfigurationManager.AppSettings["UserID"];
+            var validPWd = WebConfigurationManager.AppSettings["Pwd"];
+
+            var httpResponseMsg = new HttpResponseMessage();
+
+            if (validUserId.Equals(userID) && validPWd.Equals(pwd))
+            {
+                httpResponseMsg.Content = new StringContent("Valid user info");
+            }
+            else
+            {
+                httpResponseMsg.Content = new StringContent("ERROR: Invalid user info");
+            }
+
+            return httpResponseMsg;
         }
 
         public HttpResponseMessage Put()
