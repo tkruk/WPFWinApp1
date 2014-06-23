3e3105f27fd7a25a49af2375ad5bd7fa4175c534
tkruk <tjk_2@msn.com>
2014-06-22 20:10:57 -0400

    VS2013

---
+++ WebClient/Controllers/PracticeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebClient.Controllers
+{
+    public class PracticeController : Controller
+    {
+        //
+        // GET: /Practice/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
