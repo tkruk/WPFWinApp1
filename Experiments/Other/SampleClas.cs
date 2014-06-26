d2158b6bc3a9139d6df2fb197e54772fc1ec86c1
tkruk <tjk_2@msn.com>
2014-06-26 19:17:32 -0400

    Added new class

---
+++ Experiments/Other/SampleClas.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Experiments.Other
+{
+    public class SampleClass
+    {
+        public static int X = Y;
+        public static int Y = 3;
+    }
+}
---
+++ UnitTestExperiments/Other/UnitTestSampleClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Experiments.Other;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestExperiments.Other
+{
+    [TestClass]
+    public class UnitTestSampleClass
+    {
+        [TestMethod]
+        public void TestingStaticFields()
+        {
+            var sampleClass = new SampleClass();
+
+            Assert.AreEqual(0, SampleClass.X);
+            Assert.AreEqual(3, SampleClass.Y);
+        }
+    }
+}
